9bc777988aea558095af7c4f2c1a3199f0af6621
Bryan Mullen <bryan@bryanmullen.me>
2020-07-08 21:39:45 +0100

    Add CSS Hover Rule To Display Hard Coded Stats
    This commit adds CSS rules which upon hovering over a given character
    will display the relevent stats.
    
    Additionally it also adds logic to launch a new function startGame with
    the chosen character upon clicking.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,7 @@
 const mainContent = document.querySelector("#main-content");
-
+startGame = (character) => {
+  console.log(character);
+};
 const game = {
   titleScreen() {
     mainContent.innerHTML = `
@@ -16,24 +18,56 @@ const game = {
     mainContent.innerHTML = `
     <h2 class="character-heading">Choose Your Hero</h2>
     <div class="character-selection">
-      <div class="character-card">
+      <div class="character-card bjorna">
         <img src="assets/img/bjorna.png" alt="bjorna" />
+        <div class="stats">
+        <h3>Bjorna</h3>
+          <p>Name: Bjorna</p>
+          <p>Strength: 100</p>
+          <p>Health: 200</p>
+        </div>
         <h2>Bjorna</h2>
       </div>
-      <div class="character-card">
+      <div class="character-card jayna">
         <img src="assets/img/jayna.png" alt="jayna" />
+        <div class="stats">
+        <h3>Jayna</h3>
+          <p>Name: Jayna</p>
+          <p>Strength: 100</p>
+          <p>Health: 200</p>
+        </div>
         <h2>Lady Jayna</h2>
       </div>
-      <div class="character-card">
+      <div class="character-card yolo">
+      <div class="stats">
+          <h3>Yolo</h3>
+          <p>Name: Yolo</p>
+          <p>Strength: 100</p>
+          <p>Health: 200</p>
+        </div>
         <img src="assets/img/yolo.png" alt="yolo" />
         <h2>Yolo</h2>
       </div>
-      <div class="character-card">
+      <div class="character-card zazzerpan">
+      <div class="stats">
+          <h3>Zazzerpan</h3>
+          <p>Name: Zazzerpan</p>
+          <p>Strength: 100</p>
+          <p>Health: 200</p>
+        </div>
         <img src="assets/img/zazzerpan.png" alt="zazzerpan" />
         <h2>Zazzerpan</h2>
       </div>
     </div>
     `;
+    const bjorna = document.querySelector(".character-card.bjorna");
+    const jayna = document.querySelector(".character-card.jayna");
+    const yolo = document.querySelector(".character-card.yolo");
+    const zazzerpan = document.querySelector(".character-card.zazzerpan");
+    bjorna.addEventListener("click", startGame.bind(null, bjorna));
+    jayna.addEventListener("click", startGame.bind(null, bjorna));
+    yolo.addEventListener("click", startGame.bind(null, bjorna));
+    zazzerpan.addEventListener("click", startGame.bind(null, bjorna));
   },
   story() {},
   battle() {},
