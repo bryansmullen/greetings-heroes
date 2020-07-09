0c4583130aa0e1685e5ccfd2e0611fa0bad98ffc
Bryan Mullen <bryan@bryanmullen.me>
2020-07-09 17:56:18 +0100

    Add Story Narrative To Game Loop
    This commit adds the story narrative into the game loop and ensures the
    correct number of story items and battle items flow into each other in
    the correct order.

--- assets/js/game.js
+++ assets/js/game.js
@@ -152,7 +152,7 @@ battle = {
     `;
     document
       .getElementById("next-scene")
-      .addEventListener("click", story.sceneFive);
+      .addEventListener("click", story.sceneFour);
   },
   secondEnemy() {
     mainContent.innerHTML = `<h2>This will be the battle screen with enemy 2</h2>
@@ -161,7 +161,7 @@ battle = {
     `;
     document
       .getElementById("next-scene")
-      .addEventListener("click", story.sceneSeven);
+      .addEventListener("click", story.sceneEight);
   },
   thirdEnemy() {
     mainContent.innerHTML = `<h2>This will be the battle screen with enemy 3</h2>
@@ -177,7 +177,8 @@ battle = {
 // STORY OBJECT - DISPLAYS NARRATIVE TO SCREEN
 const story = {
   sceneOne() {
-    mainContent.innerHTML = `<h2>This is scene one</h2> 
+    mainContent.innerHTML = `<p class="story">A dread shadow stalks the land! Arch-Wizard Valderak, freed from his timeless prison, has come to wreak vengeance on the people of The Four Kingdoms.
+    </p>
     <div id="next-scene" class="button">Progress to next scene</div>
     `;
     document
@@ -185,7 +186,8 @@ const story = {
       .addEventListener("click", story.sceneTwo);
   },
   sceneTwo() {
-    mainContent.innerHTML = `<h2>This is scene two</h2> 
+    mainContent.innerHTML = `<p class="story">
+    From his tower deep in the Forest of Poison, he plots a terrible revenge.
     <div id="next-scene" class="button">Progress to next scene</div>
     `;
     document
@@ -193,23 +195,26 @@ const story = {
       .addEventListener("click", story.sceneThree);
   },
   sceneThree() {
-    mainContent.innerHTML = `<h2>This is scene three</h2> 
-    <div id="next-scene" class="button">Progress to next scene</div>
-    `;
-    document
-      .getElementById("next-scene")
-      .addEventListener("click", story.sceneFour);
-  },
-  sceneFour() {
-    mainContent.innerHTML = `<h2>This is scene four</h2> 
+    mainContent.innerHTML = `<p class="story">
+    Four heroes, the greatest from each of the Kingdoms, have been sent to thwart the schemes of the Arch-Wizard and restore peace to the land.</p>
     <div id="next-scene" class="button">Progress to next scene</div>
     `;
     document
       .getElementById("next-scene")
       .addEventListener("click", battle.firstEnemy);
   },
+  sceneFour() {
+    mainContent.innerHTML = `<p class="story">You and your party make your through the Forest of Poison, through foul waters and rotting trees. 
+</p>
+    <div id="next-scene" class="button">Progress to next scene</div>
+    `;
+    document
+      .getElementById("next-scene")
+      .addEventListener("click", story.sceneFive);
+  },
   sceneFive() {
-    mainContent.innerHTML = `<h2>This is scene Five</h2> 
+    mainContent.innerHTML = `<p class="story">Before you, rising like a thorn from the putrid mire, stands the dark tower of Valderak, its arched windows filled with the green fire of his baleful magics.
+</p>
     <div id="next-scene" class="button">Progress to next scene</div>
     `;
     document
@@ -217,7 +222,8 @@ const story = {
       .addEventListener("click", story.sceneSix);
   },
   sceneSix() {
-    mainContent.innerHTML = `<h2>This is scene Six</h2> 
+    mainContent.innerHTML = `<p class="story">You step inside the echoing tower and encounter the first of line of Valderak's defenses - the people of the forest, now corrupted by Valderak.
+</p>
     <div id="next-scene" class="button">Progress to next scene</div>
     `;
     document
@@ -225,7 +231,53 @@ const story = {
       .addEventListener("click", battle.secondEnemy);
   },
   sceneSeven() {
-    mainContent.innerHTML = `<h2>This is scene Seven</h2> 
+    mainContent.innerHTML = `<p class="story">Defeating the forest folk, you are able to free them from the influence of Valderak. Before fleeing the terrible tower they give you the key to the upper floors. Ascending the winding stairs your ears pick up a harsh, clanking sound. There is smoke on the air. Opening a door to an upper chamber, you encounter Valderak's lieutenant, an Engine of Chaos.
+</p>
+    <div id="next-scene" class="button">Progress to next scene</div>
+    `;
+    document
+      .getElementById("next-scene")
+      .addEventListener("click", story.sceneFour);
+  },
+  sceneEight() {
+    mainContent.innerHTML = `<p class="story">The Engine of Chaos is destroyed and from its wreckage you pluck the source of its power, a Gem of Annihilation.
+</p>
+    <div id="next-scene" class="button">Progress to next scene</div>
+    `;
+    document
+      .getElementById("next-scene")
+      .addEventListener("click", story.sceneNine);
+  },
+  sceneNine() {
+    mainContent.innerHTML = `<p class="story">Using it you dispel the wards and magics protecting the inner sanctum of Valderak, allowing you to enter the lair of the Arch-Wizard. 
+</p>
+    <div id="next-scene" class="button">Progress to next scene</div>
+    `;
+    document
+      .getElementById("next-scene")
+      .addEventListener("click", story.sceneTen);
+  },
+  sceneTen() {
+    mainContent.innerHTML = `<p class="story">You find him, hunched and wizened over his scrying table. 
+</p>
+    <div id="next-scene" class="button">Progress to next scene</div>
+    `;
+    document
+      .getElementById("next-scene")
+      .addEventListener("click", story.sceneEleven);
+  },
+  sceneEleven() {
+    mainContent.innerHTML = `<p class="story">With a screech of disgust he conjures his Poison Blade, and with one final curse on the Four Kingdoms, launches himself at you.
+</p>
+    <div id="next-scene" class="button">Progress to next scene</div>
+    `;
+    document
+      .getElementById("next-scene")
+      .addEventListener("click", story.sceneTwelve);
+  },
+  sceneTwelve() {
+    mainContent.innerHTML = `<p class="story">Battle is joined.
+</p>
     <div id="next-scene" class="button">Progress to next scene</div>
     `;
     document
@@ -237,21 +289,77 @@ const story = {
     <div id="victory" class="button">Victory</div>
     <div id="game-over" class="button">Game Over</div>
     `;
-    document.getElementById("victory").addEventListener("click", story.victory);
+    document
+      .getElementById("victory")
+      .addEventListener("click", story.victoryOne);
     document
       .getElementById("game-over")
-      .addEventListener("click", story.gameOver);
+      .addEventListener("click", story.gameOverOne);
   },
-  gameOver() {
+  gameOverOne() {
     mainContent.innerHTML = `<h2>Game Over</h2> 
+    <p class="story">All your valour, all your bravery, was for naught.
+</p>
+    <div id="return-to-title" class="button">Progress To Next Scene</div>
+    `;
+    document
+      .getElementById("return-to-title")
+      .addEventListener("click", story.gameOverTwo);
+  },
+  gameOverTwo() {
+    mainContent.innerHTML = `<h2>Game Over</h2> 
+    <p class="story">You have been vanquished.
+</p>
+    <div id="return-to-title" class="button">Progress To Next Scene</div>
+    `;
+    document
+      .getElementById("return-to-title")
+      .addEventListener("click", story.gameOverThree);
+  },
+  gameOverThree() {
+    mainContent.innerHTML = `<h2>Game Over</h2> 
+    <p class="story">It is a dark day for the Four Kingdoms as its greatest champions lie defeated, but there will be many dark days ahead, for now nothing stands in the way of Valderak's final victory.
+</p>
+    <div id="return-to-title" class="button">Progress To Next Scene</div>
+    `;
+    document
+      .getElementById("return-to-title")
+      .addEventListener("click", story.gameOverFour);
+  },
+  gameOverFour() {
+    mainContent.innerHTML = `<h2>Game Over</h2> 
+    <p class="story">All is lost.
+</p>
     <div id="return-to-title" class="button">Return To Title Screen</div>
     `;
     document
       .getElementById("return-to-title")
       .addEventListener("click", game.titleScreen);
   },
-  victory() {
+  victoryOne() {
     mainContent.innerHTML = `<h2>You Won!</h2> 
+    <p class="story">With a blood-curdling scream, the magics holding the Arch-Wizard together are sundered for good- in a great rush of cold green fire, the body falls, nothing more than a few rags and bones. The dark tower begins to shudder beneath your feet - without Valderak to maintain it, it is beginning to sink into the swamp.
+</p>
+    <div id="return-to-title" class="button">Progress To Next Scene</div>
+    `;
+    document
+      .getElementById("return-to-title")
+      .addEventListener("click", story.victoryTwo);
+  },
+  victoryTwo() {
+    mainContent.innerHTML = `<h2>You Won!</h2> 
+    <p class="story">You fly for the exit, escaping just in time to see the sharp roof sink beneath the waters. 
+</p>
+    <div id="return-to-title" class="button">Progress To Next Scene</div>
+    `;
+    document
+      .getElementById("return-to-title")
+      .addEventListener("click", story.victoryThree);
+  },
+  victoryThree() {
+    mainContent.innerHTML = `<h2>You Won!</h2> 
+    <p class="story">The stain on the landscape, the blight on the Four Kingdoms, Valderak the Arch-Wizard is no more.
+</p>
     <div id="return-to-title" class="button">Return To Title Screen</div>
     `;
     document
