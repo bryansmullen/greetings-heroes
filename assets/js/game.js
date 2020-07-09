d89668700ddbb5e38cc07c7125df948a713813a8
Bryan Mullen <bryan@bryanmullen.me>
2020-07-09 03:57:29 +0100

    Add Event Listeners For Basic Functionality - Play, Info, Music, Exit
    This commit adds event listeners for basic interaction with the user
    interface, allowing the toggling of play, exiting, info and music.
    
    The exit to title method is removed and incorportated into the
    titleScreen Method, which will form more of an initialization role now.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,78 @@
+// GLOBAL CONSTANTS
 const mainContent = document.querySelector("#main-content");
+
+// GLOBAL FUNCTIONS
 startGame = (character) => {
   console.log(character);
 };
+
+// GAME OBJECT
 const game = {
+  // TITLE SCREEN METHOD - RESETS GAME TO TITLE SCREEN
   titleScreen() {
     mainContent.innerHTML = `
     <div id="play-game-button" class="button">Play Game</div>
     <div id="instructions-button" class="button">Instructions</div>
     `;
-    const playGameButton = document.querySelector("#play-game-button");
-    playGameButton.addEventListener("click", this.chooseCharacter);
+    document
+      .querySelector("#play-game-button")
+      .addEventListener("click", game.chooseCharacter);
+
+    // THESE INITIALISE HEADER UI
+    document
+      .getElementById("instructions-button")
+      .addEventListener("click", game.information);
+    document.getElementById("info").addEventListener("click", game.information);
+
+    document
+      .getElementById("exit-game")
+      .addEventListener("click", game.titleScreen);
+
+    document
+      .getElementById("toggle-sound")
+      .addEventListener("click", game.toggleSound);
   },
-  information() {},
+
+  // INFORMATION METHOD - DISPLAYS INSTRUCTIONS
+  information() {
+    mainContent.innerHTML = `
+    <h2>Instructions</h2>
+      <p>
+        The game is played using only the mouse.
+      </p>
+
+      <p>
+        To begin, click on the "Start Game" button on the main menu. You will be
+        brought to the character selection screen
+      </p>
+      <p>
+        You may inspect the attributes of each of the characters by hovering
+        over each of them in turn, and when you have decided which character you
+        would like to play as, simply click on them to select, and confirm your
+        choice.
+      </p>
+      <p>
+        The game will begin, and you may advance through the story by clicking
+        the "next" button in the story window. When battles commence you may
+        choose from a list of commands - attack, magic, and item.
+      </p>
+      <p>
+        Once you make your selection your character will take their turn, after
+        which the computer gets a chance to retaliate. Both the player and the
+        computer take it in turns to use a command of their choice until one
+        person wins.
+      </p>
+      <p>
+        If the player wins, you will progress through the story until you defeat
+        the final enemy.
+      </p>
+    `;
+  },
+
+  // TOGGLE SOUND METHOD - MUTES/UNMUTES MUSIC
   toggleSound() {},
-  exitToTitle() {},
+
+  // CHOOSE CHARACTER - ALLOWS GAME TO BE STARTED WITH CHOICE OF CHARACTER
   chooseCharacter() {
     mainContent.innerHTML = `
     <h2 class="character-heading">Choose Your Hero</h2>
@@ -69,7 +128,11 @@ const game = {
     yolo.addEventListener("click", startGame.bind(null, bjorna));
     zazzerpan.addEventListener("click", startGame.bind(null, bjorna));
   },
+
+  // STORY METHOD - DISPLAYS NARRATIVE TO SCREEN
   story() {},
+
+  // BATTLE METHOD - DISPLAYS BATTLE UI
   battle() {},
 };
 
