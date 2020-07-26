0dff627350bf496784adbae8df353f93751c34c9
Bryan Mullen <bryan@bryanmullen.me>
2020-07-26 13:02:10 +0100

    Begin Add Updated Story
    This commit begins to add updated story narrative to allow layout to be
    built around it

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,253 +6,399 @@ export class Game {
     this.character = undefined;
   }
   toggleSoundIcon() {
-    const soundIcon = document.getElementById('toggle-sound');
-    if (soundIcon.classList.contains('fa-volume-mute')) {
-      soundIcon.classList.remove('fa-volume-mute')
-      soundIcon.classList.add('fa-volume-up')
+    const soundIcon = document.getElementById("toggle-sound");
+    if (soundIcon.classList.contains("fa-volume-mute")) {
+      soundIcon.classList.remove("fa-volume-mute");
+      soundIcon.classList.add("fa-volume-up");
     } else {
-      soundIcon.classList.remove('fa-volume-up')
-      soundIcon.classList.add('fa-volume-mute')
+      soundIcon.classList.remove("fa-volume-up");
+      soundIcon.classList.add("fa-volume-mute");
     }
   }
   toggleAudio() {
-    const audio = document.querySelector('audio');
+    const audio = document.querySelector("audio");
     if (audio.muted) {
       audio.muted = false;
     } else {
       audio.muted = true;
     }
-    this.toggleSoundIcon()
+    this.toggleSoundIcon();
   }
   titleScreen() {
-    updateUi('title-screen')
-    document.getElementById('play-game-button').addEventListener('click', () => {
-      this.chooseCharacter();
-    })
-    document.getElementById('exit-game').addEventListener('click', () => {
-      this.titleScreen()
-    })
-    document.getElementById('instructions-button').addEventListener('click', () => {
-      this.instructionsScreen()
-    })
-    document.getElementById('info-button').addEventListener('click', () => {
-      this.instructionsScreen()
-    })
+    updateUi("title-screen");
+    document
+      .getElementById("play-game-button")
+      .addEventListener("click", () => {
+        this.prelude();
+      });
+    document.getElementById("exit-game").addEventListener("click", () => {
+      this.titleScreen();
+    });
+    document
+      .getElementById("instructions-button")
+      .addEventListener("click", () => {
+        this.instructionsScreen();
+      });
+    document.getElementById("info-button").addEventListener("click", () => {
+      this.instructionsScreen();
+    });
   }
   instructionsScreen() {
-    updateUi('instructions-screen')
-    document.getElementById('return-to-title').addEventListener('click', () => {
-      this.titleScreen()
-    })
+    updateUi("instructions-screen");
+    document.getElementById("return-to-title").addEventListener("click", () => {
+      this.titleScreen();
+    });
   }
   chooseCharacter() {
-    updateUi('character-screen')
-    audio.play('1m01')
-    this.toggleSoundIcon()
-    document.getElementById('bjorna').addEventListener('click', () => {
-      this.character = 'bjorna'
+    updateUi("character-screen");
+    audio.play("1m01");
+    this.toggleSoundIcon();
+    document.getElementById("bjorna").addEventListener("click", () => {
+      this.character = "bjorna";
+      this.bjorna();
+    });
+    document.getElementById("jayna").addEventListener("click", () => {
+      this.character = "jayna";
 
-      this.stage1();
-    })
-    document.getElementById('jayna').addEventListener('click', () => {
-      this.character = 'jayna'
+      this.jayna();
+    });
+    document.getElementById("yolo").addEventListener("click", () => {
+      this.character = "yolo";
 
-      this.stage1();
-    })
-    document.getElementById('yolo').addEventListener('click', () => {
-      this.character = 'yolo'
+      this.yolo();
+    });
+    document.getElementById("zazzerpan").addEventListener("click", () => {
+      this.character = "zazzerpan";
 
+      this.zazzerpan();
+    });
+  }
+  bjorna() {
+    updateUi("choose-bjorna");
+    document.getElementById("bjorna-progress").addEventListener("click", () => {
       this.stage1();
-    })
-    document.getElementById('zazzerpan').addEventListener('click', () => {
-      this.character = 'zazzerpan'
-
+    });
+  }
+  jayna() {
+    updateUi("choose-jayna");
+    document.getElementById("jayna-progress").addEventListener("click", () => {
       this.stage1();
-    })
+    });
+  }
+  yolo() {
+    updateUi("choose-yolo");
+    document.getElementById("yolo-progress").addEventListener("click", () => {
+      this.stage1();
+    });
+  }
+  zazzerpan() {
+    updateUi("choose-zazzerpan");
+    document
+      .getElementById("zazzerpan-progress")
+      .addEventListener("click", () => {
+        this.stage1();
+      });
+  }
+  prelude() {
+    updateUi("prelude");
+    document
+      .getElementById("prelude-progress")
+      .addEventListener("click", () => {
+        this.chooseCharacter();
+      });
   }
   stage1() {
-    updateUi('first-story')
-    document.getElementById('scene-one-progress').addEventListener('click', () => {
-      this.stage2();
-    })
+    updateUi("first-story");
+    document
+      .getElementById("scene-one-progress")
+      .addEventListener("click", () => {
+        const diceRoll = Math.ceil(Math.random() * 3);
+        this.stage2(diceRoll);
+      });
   }
-  stage2() {
-    updateUi('second-story')
-    document.getElementById('scene-two-progress').addEventListener('click', () => {
-      this.stage3();
-    })
+  stage2(diceRoll) {
+    switch (diceRoll) {
+      case 1:
+        updateUi("second-story-a");
+        document
+          .getElementById("scene-two-a-progress")
+          .addEventListener("click", () => {
+            this.battle1("forest");
+          });
+        break;
+      case 2:
+        updateUi("second-story-b");
+        document
+          .getElementById("scene-two-b-progress")
+          .addEventListener("click", () => {
+            this.battle1("melwunt");
+          });
+        break;
+      default:
+        updateUi("second-story-c");
+        document
+          .getElementById("scene-two-c-progress")
+          .addEventListener("click", () => {
+            this.battle1("wretcheddead");
+          });
+        break;
+    }
   }
-  stage3() {
-    updateUi('third-story')
-    document.getElementById('scene-three-progress').addEventListener('click', () => {
-      this.stage4();
-    })
+  stage3(enemyDefeated) {
+    switch (enemyDefeated) {
+      case "a":
+        updateUi("third-story-a");
+        break;
+      case "b":
+        updateUi("third-story-b");
+        break;
+      case "c":
+        updateUi("third-story-c");
+        break;
+    }
+    const reward = Math.ceil(Math.random() * 3);
+    document
+      .getElementById("scene-three-a-progress")
+      .addEventListener("click", () => {
+        this.stage4(reward);
+      });
+    document
+      .getElementById("scene-three-b-progress")
+      .addEventListener("click", () => {
+        this.stage4(reward);
+      });
+    document
+      .getElementById("scene-three-c-progress")
+      .addEventListener("click", () => {
+        this.stage4(reward);
+      });
   }
-  stage4() {
-    updateUi('fourth-story')
-    document.getElementById('scene-four-progress').addEventListener('click', () => {
-      this.stage5();
-    })
+  stage4(reward) {
+    switch (reward) {
+      case 1:
+        updateUi("fourth-story-a");
+        break;
+      case 2:
+        updateUi("fourth-story-b");
+        break;
+      case 3:
+        updateUi("fourth-story-c");
+        break;
+    }
+    document
+      .getElementById("scene-four-progress")
+      .addEventListener("click", () => {
+        this.stage5();
+      });
   }
   stage5() {
-    updateUi('fifth-story')
-    document.getElementById('scene-five-progress').addEventListener('click', () => {
-      this.stage6();
-    })
+    updateUi("fifth-story");
+    document
+      .getElementById("scene-five-progress")
+      .addEventListener("click", () => {
+        this.stage6();
+      });
   }
   stage6() {
-    updateUi('sixth-story')
-    document.getElementById('scene-six-progress').addEventListener('click', () => {
-      this.battle1();
-    })
+    updateUi("sixth-story");
+    document
+      .getElementById("scene-six-progress")
+      .addEventListener("click", () => {
+        this.battle1();
+      });
   }
   stage7() {
-    updateUi('seventh-story')
-    audio.play('1m02')
-    document.getElementById('scene-seven-progress').addEventListener('click', () => {
-      this.battle2();
-    })
+    updateUi("seventh-story");
+    audio.play("1m02");
+    document
+      .getElementById("scene-seven-progress")
+      .addEventListener("click", () => {
+        this.battle2();
+      });
   }
   stage8() {
-    updateUi('eighth-story')
-    audio.play('1m03')
-    document.getElementById('scene-eight-progress').addEventListener('click', () => {
-      this.stage9();
-    })
+    updateUi("eighth-story");
+    audio.play("1m03");
+    document
+      .getElementById("scene-eight-progress")
+      .addEventListener("click", () => {
+        this.stage9();
+      });
   }
   stage9() {
-    updateUi('ninth-story')
-    document.getElementById('scene-nine-progress').addEventListener('click', () => {
-      this.stage10();
-    })
+    updateUi("ninth-story");
+    document
+      .getElementById("scene-nine-progress")
+      .addEventListener("click", () => {
+        this.stage10();
+      });
   }
   stage10() {
-    updateUi('tenth-story')
-    document.getElementById('scene-ten-progress').addEventListener('click', () => {
-      this.stage11();
-    })
+    updateUi("tenth-story");
+    document
+      .getElementById("scene-ten-progress")
+      .addEventListener("click", () => {
+        this.stage11();
+      });
   }
   stage11() {
-    updateUi('eleventh-story')
-    document.getElementById('scene-eleven-progress').addEventListener('click', () => {
-      this.stage12();
-    })
+    updateUi("eleventh-story");
+    document
+      .getElementById("scene-eleven-progress")
+      .addEventListener("click", () => {
+        this.stage12();
+      });
   }
   stage12() {
-    updateUi('twelfth-story')
-    audio.play('1m04')
-    document.getElementById('scene-twelve-progress').addEventListener('click', () => {
-      this.battle3();
-    })
+    updateUi("twelfth-story");
+    audio.play("1m04");
+    document
+      .getElementById("scene-twelve-progress")
+      .addEventListener("click", () => {
+        this.battle3();
+      });
   }
   finalStage() {
-    updateUi('final-scene')
-    document.getElementById('victory').addEventListener('click', () => {
+    updateUi("final-scene");
+    document.getElementById("victory").addEventListener("click", () => {
       this.victoryOne();
-    })
-    document.getElementById('game-over').addEventListener('click', () => {
+    });
+    document.getElementById("game-over").addEventListener("click", () => {
       this.gameOverOne();
-    })
+    });
   }
   victoryOne() {
-    updateUi('victory-one')
-    document.getElementById('victory-one-progress').addEventListener('click', () => {
-      this.victoryTwo();
-    })
+    updateUi("victory-one");
+    document
+      .getElementById("victory-one-progress")
+      .addEventListener("click", () => {
+        this.victoryTwo();
+      });
   }
 
   victoryTwo() {
-    updateUi('victory-two')
-    document.getElementById('victory-two-progress').addEventListener('click', () => {
-      this.victoryThree();
-    })
+    updateUi("victory-two");
+    document
+      .getElementById("victory-two-progress")
+      .addEventListener("click", () => {
+        this.victoryThree();
+      });
   }
   victoryThree() {
-    updateUi('victory-three')
-    document.getElementById('victory-three-progress').addEventListener('click', () => {
-      this.titleScreen();
-    })
+    updateUi("victory-three");
+    document
+      .getElementById("victory-three-progress")
+      .addEventListener("click", () => {
+        this.titleScreen();
+      });
   }
   gameOverOne() {
-    updateUi('game-over-one')
-    document.getElementById('game-over-one-progress').addEventListener('click', () => {
-      this.gameOverTwo();
-    })
+    updateUi("game-over-one");
+    document
+      .getElementById("game-over-one-progress")
+      .addEventListener("click", () => {
+        this.gameOverTwo();
+      });
   }
   gameOverTwo() {
-    updateUi('game-over-two')
-    document.getElementById('game-over-two-progress').addEventListener('click', () => {
-      this.gameOverThree();
-    })
+    updateUi("game-over-two");
+    document
+      .getElementById("game-over-two-progress")
+      .addEventListener("click", () => {
+        this.gameOverThree();
+      });
   }
   gameOverThree() {
-    updateUi('game-over-three')
-    document.getElementById('game-over-three-progress').addEventListener('click', () => {
-      this.gameOverFour();
-    })
+    updateUi("game-over-three");
+    document
+      .getElementById("game-over-three-progress")
+      .addEventListener("click", () => {
+        this.gameOverFour();
+      });
   }
   gameOverFour() {
-    updateUi('game-over-four')
-    document.getElementById('game-over-four-progress').addEventListener('click', () => {
-      this.titleScreen();
-    })
+    updateUi("game-over-four");
+    document
+      .getElementById("game-over-four-progress")
+      .addEventListener("click", () => {
+        this.titleScreen();
+      });
   }
-  battle1() {
-    updateUi('battle-one')
+  battle1(enemy) {
+    console.log(enemy);
+    switch (enemy) {
+      case "forest":
+        updateUi("battle-forest");
+        break;
+      case "melwunt":
+        updateUi("battle-melwunt");
+      case "wretcheddead":
+        updateUi("battle-wretcheddead");
+    }
     // ==========================================================//
 
-    const attack = document.querySelector('.attack')
-    attack.addEventListener('click', () => { console.log('attack') })
-    const defend = document.querySelector('.defend')
-    defend.addEventListener('click', () => { console.log('defend') })
-    const special = document.querySelector('.special')
-    special.addEventListener('click', () => { console.log('special') })
-    const playerHealthReadout = document.getElementById('player-health')
-    const enemyHealthReadout = document.getElementById('enemy-health')
+    const attack = document.querySelector(".attack");
+    attack.addEventListener("click", () => {
+      console.log("attack");
+    });
+    const defend = document.querySelector(".defend");
+    defend.addEventListener("click", () => {
+      console.log("defend");
+    });
+    const special = document.querySelector(".special");
+    special.addEventListener("click", () => {
+      console.log("special");
+    });
+    const playerHealthReadout = document.getElementById("player-health");
+    const enemyHealthReadout = document.getElementById("enemy-health");
 
     const player = {
       healthStat: 100,
       defenceStat: 50,
       attackStat: 70,
       attack() {
-        enemy.health -= 10
-        console.dir(enemyHealthReadout)
-
-      }
-    }
-    const enemy = {
-      health: 60,
-      defence: 40,
-      attack: 50
-    }
-
-
-
-
-
-
-
-
+        enemy.health -= 10;
+        console.dir(enemyHealthReadout);
+      },
+    };
+    // const enemy = {
+    //   health: 60,
+    //   defence: 40,
+    //   attack: 50,
+    // };
 
     // ==========================================================//
 
-
-    document.getElementById('battle-one-progress').addEventListener('click', () => {
-      this.stage7();
-    })
+    document
+      .querySelector(".battle-one-progress-a")
+      .addEventListener("click", () => {
+        this.stage3("a");
+      });
+    document
+      .querySelector(".battle-one-progress-b")
+      .addEventListener("click", () => {
+        this.stage3("b");
+      });
+    document
+      .querySelector(".battle-one-progress-c")
+      .addEventListener("click", () => {
+        this.stage3("c");
+      });
   }
   battle2() {
-    updateUi('battle-two')
-    document.getElementById('battle-two-progress').addEventListener('click', () => {
-      this.stage8();
-    })
+    updateUi("battle-two");
+    document
+      .getElementById("battle-two-progress")
+      .addEventListener("click", () => {
+        this.stage8();
+      });
   }
   battle3() {
-    updateUi('battle-three')
-    document.getElementById('battle-three-progress').addEventListener('click', () => {
-      this.finalStage();
-    })
+    updateUi("battle-three");
+    document
+      .getElementById("battle-three-progress")
+      .addEventListener("click", () => {
+        this.finalStage();
+      });
   }
 }
-
-
