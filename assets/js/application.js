8cfe5b9074f1b3936c2bbef883f823a8cef7e880
Bryan Mullen <bryan@bryanmullen.me>
2020-07-31 22:00:30 +0100

    tutor support update

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,8 +1,16 @@
 import { Game } from "./game.js";
+import { Player, Enemy, Battle } from "./battle.js";
+import { story } from "./story.js";
 
 const myGame = new Game();
-document.getElementById("toggle-sound").addEventListener("click", () => {
-  myGame.toggleAudio();
-});
-myGame.titleScreen();
-// myGame.battle1();
+
+myGame.start = () => {
+  // Start Game Running
+  myGame.stage = "1";
+  if (myGame.stage == "1") {
+    myGame.renderUi("1");
+  }
+  myGame.readyProgress();
+};
+
+myGame.start();
--- assets/js/battle.js
+++ assets/js/battle.js
@@ -21,3 +21,62 @@ export class Enemy {
     this.maxHealth = health;
   }
 }
+
+// Battle Logic
+
+export class Battle {
+  constructor(enemy, attackBtn, defendBtn, specialBtn, battleUi, progressBtn, nextStage, enemyHealthBarId, option) {
+    this.attackBtn = attackBtn;
+    this.defendBtn = defendBtn;
+    this.specialBtn = specialBtn;
+    this.battleUi = battleUi;
+    this.progressBtn = progressBtn;
+    this.nextStage = nextStage;
+    this.option = option;
+    this.enemy = enemy;
+    this.enemyHealthBarId = enemyHealthBarId;
+  }
+  updateHealth() {
+    enemy.health -= 10;
+  }
+  initialiseBattleCommand(attackBtn, defendBtn, specialBtn) {
+    attackBtn.addEventListener("click", () => {
+      this.playerMove(this.enemy, "forest-people-health", "attack");
+      this.checkHealth(this.progressBtn);
+    });
+    defendBtn.addEventListener("click", () => {
+      this.playerMove(enemy, "forest-people-health", "attack");
+      this.checkHealth(this.progressBtn);
+    });
+    specialBtn.addEventListener("click", () => {
+      this.playerMove(enemy, "forest-people-health", "attack");
+      this.checkHealth(this.progressBtn);
+    });
+  }
+
+  playerMove(enemy, enemyHealthBar, choice) {
+    switch (choice) {
+      case "attack":
+        enemy.health -= 10;
+        enemyHealthBar = document.getElementById(this.enemyHealthBarId);
+        enemyHealthBar.value = (enemy.health / enemy.maxHealth) * 100;
+        break;
+      case "defend":
+        console.log("defend");
+        break;
+      case "special":
+        console.log("special");
+        break;
+    }
+  }
+  checkHealth(progressBtn) {
+    console.dir(progressBtn);
+    if (this.enemy.health <= 0) {
+      document.getElementById(progressBtn).classList.remove("hidden");
+    }
+  }
+  run() {
+    this.initialiseBattleCommand(this.attackBtn, this.defendBtn, this.specialBtn);
+    updateAndListen(this.battleUi, this.progressBtn, this.nextStage, this.option);
+  }
+}
