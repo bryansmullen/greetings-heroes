73ffe0387acc975089350b0587c01be51f31ce68
Bryan Mullen <bryan@bryanmullen.me>
2020-08-01 22:20:34 +0100

    Convert Audio From Object To Function
    This commit begins the process of simplifying the application by
    converting the audio object which only had one method into a function

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,13 +1,19 @@
-export const audio = {
-    play(cue) {
-        let currentlyPlaying = document.querySelector('audio');
-        if (currentlyPlaying) {
-            currentlyPlaying.remove();
-        }
-        let newTrack = document.createElement("audio");
-        newTrack.loop = true;
-        newTrack.src = `assets/audio/${cue}.mp3`;
-        newTrack.play()
-        document.body.appendChild(newTrack)
-    }
-}
+/**
+ * Plays Audio In the Browser.
+ *
+ * This function takes one argument - the cue name of the track to be played. The cue should exist in the assets/audio/ directory and be in .mp3 format
+ *
+ * @param {string}   string      The audio cue to be played, formatted as .mp3
+ *
+ */
+export const audio = function (cue) {
+  let currentlyPlaying = document.querySelector("audio");
+  if (currentlyPlaying) {
+    currentlyPlaying.remove();
+  }
+  let newTrack = document.createElement("audio");
+  newTrack.loop = true;
+  newTrack.src = `assets/audio/${cue}.mp3`;
+  newTrack.play();
+  document.body.appendChild(newTrack);
+};
