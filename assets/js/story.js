d9be4d5126b90cccca2dacf3b1569db16b9f42ea
Bryan Mullen <bryan@bryanmullen.me>
2020-08-01 18:39:01 +0100

    Move Narrative To Story Module
    This commit moves the content that is largely hidden in the index.html
    file into a separate story.js module, which will be pulled from and
    rendered as required

--- assets/js/story.js
+++ assets/js/story.js
@@ -1,47 +1,225 @@
-export const story = {
-  "1":
-    " You and your party make your through the Forest of Poison, through foul waters and rotting trees. Before you, rising like a thorn from the putrid mire, stands the dark tower of Valderak. Its arched windows are filled with the green fire of his baleful magics. You step inside the echoing tower. The first of line of Valderak's defenses falls upon you:",
-  "2a": "the people of the forest, now corrupted by Valderak.",
-  "2b": "a local tribe of Melwunts, their nasty natures amplified by Valderak",
-  "2c": "a group of the Wretched Dead, corpses animated by the magics of Valderak.",
-  "3a": "Defeating the forest folk, you are able to free them from the influence of Valderak.",
-  "3b": "Defeating the Melwunts, you send them scampering back into the marshes.",
-  "3c": "Defeating the Wretched Dead, they fall to pieces on the floor, no longer animated by Valderak.",
-  "4a": "Your foe leaves behind a powerful artefact: The Wand of Azzerthoth, crackling with violet energies [ATTACK UP]",
-  "4b": "Your foe leaves behind a powerful artefact: The Brazen Helm of Caargolos, the demon-mask of a dead god [DEFENSE UP]",
-  "4c": "Your foe leaves behind a powerful artefact: The Philtre of Gnomon - a glass vial filled with moonlight [HEALING]",
-  "5":
-    "Hidden in the wreckage you find the key to the upper floors. Ascending the winding stairs your ears pick up a harsh, clanking sound. There is smoke upon the air. You are greeted by three doors - to your left a door of ruby, to your right a door of aquamarine, and between, a door of topaz.",
-  "6a":
-    " Opening the ruby door to an upper chamber cloaked in scarlet cloth you encounter Valderak's lieutenant, an Engine of Chaos, powered by the elemental fury of fire.",
-  "6b":
-    "Opening the aquamarine door to an upper chamber riven with streams you encounter Valderak's lieutenant, an Engine of Chaos, powered by the elemental fury of water",
-  "6c":
-    "Opening the topaz door to an upper chamber choked with sand you encounter Valderak's lieutenant, an Engine of Chaos, powered by the elemental fury of lightning",
-  "7a": "The Engine of Chaos is destroyed. From its wreckage you pluck the source of its power, a Gem of Annihilation. It pulses with the power of fire",
-  "7b": "The Engine of Chaos is destroyed. From its wreckage you pluck the source of its power, a Gem of Annihilation. It pulses with the power of water",
-  "7c": "The Engine of Chaos is destroyed. From its wreckage you pluck the source of its power, a Gem of Annihilation. It pulses with the power of lightning",
-  "8":
-    "Using it you dispel the wards and magics protecting the inner sanctum of Valderak. Entering the lair of the Arch-Wizard, you find him, hunched over his scrying table. He turns and addresses you.",
-  "9a":
-    "All your efforts are in vain, little one. Aquilae will be the first to fall. Your father is doomed - and you shall live just long enough to see him perish!",
-  "9b":
-    "Foolish wretch - your tribe is lost in the seas of time! I may not have been able to best their strength, but now they are somewhere their meagre intellect will not save them. Do you truly think one of you can defeat me? After all your people joined as one, failed?",
-  "9c":
-    "You dare to disturb the dread Valderak? For what end? To thwart me in my plans? Foolish dolt - you cannot defeat me! All magic is mine! All magic flows through me!",
-  "9d":
-    "Wait. I know you. Didn't you break into my tower before? Yes! you're the one who stole my record collection! You took all my precious, priceless Bob Dylan records!",
-  "10": "With a screech of disgust he conjures his Poison Blade, and with one final curse on the Four Kingdoms, launches himself at you. Battle is joined.",
-  "11a":
-    "You return to the Kingdom of Aquilae to find the ravages of the dread Arch-Wizard fading from the land. Your father has been restored to health. With the death of his twin brother Valderak, peace now reigns... for about a fortnight until your evil twin sister escapes...",
-  "11b":
-    "In your hand you hold the Dispelling Arch, and returning to the Realm of Harthingfjord, you use it to pluck your tribe from the rivers of time. They are safe, but the Arch demands a sacrifice - you find yourself lost in time and space.",
-  "11c":
-    "In your hands the great tome Mallifax belches, swollen with the stolen magics of Valderak. But returning to Fizzlesnip, you find it in disarray. You have done your job too well - all magic has been drained from the Four Kingdoms and beyond. The world is saved but Zazzerpan, you're going to have do what your father's always said. Get a real job.",
-  "11d":
-    "You make it through the swamp with your sack of stolen artefacts. Resting for the night you look at your swag. Surely it wouldn't hurt to try on the Ring of Destruction? And maybe the Crown of Pain would compliment it... and the Cloak of Daggers too. Your team-mates raise concerns, but you wave them away, accidentally obliterating them. Your hands course with power. The dark reign of Yolo the Magnificent has begun.",
-  "Game Over":
-    "All your valour, all your bravery, was for naught. You have been vanquished. It is a dark day for the Four Kingdoms as its greatest champions lie defeated. There will be many more dark days ahead, for now nothing stands in the way of Valderak's final victory. All is lost.",
-  Victory:
-    "With a blood-curdling scream, the magics holding the Arch-Wizard together are sundered for good. In a great rush of cold green fire, the body falls, nothing more than a few rags and bones. The dark tower begins to shudder beneath your feet. Without Valderak to maintain it, it is beginning to sink into the swamp. You fly for the exit, escaping just in time to see the sharp roof sink beneath the waters. The stain on the landscape, the blight on the Four Kingdoms, Valderak the Arch-Wizard is no more.",
+export const stage = {
+  title: {
+    type: "title",
+  },
+  character: {
+    type: "character",
+    next: "1",
+  },
+  "1": {
+    text:
+      " You and your party make your through the Forest of Poison, through foul waters and rotting trees. Before you, rising like a thorn from the putrid mire, stands the dark tower of Valderak. Its arched windows are filled with the green fire of his baleful magics. You step inside the echoing tower. The first of line of Valderak's defenses falls upon you:",
+    type: "story",
+    next: "2a",
+  },
+
+  "2a": {
+    text: "the people of the forest, now corrupted by Valderak.",
+    type: "story",
+    next: "battle1a",
+  },
+  "2b": {
+    text: "a local tribe of Melwunts, their nasty natures amplified by Valderak",
+    type: "story",
+    next: "battle1b",
+  },
+  "2c": {
+    text: "a group of the Wretched Dead, corpses animated by the magics of Valderak.",
+    type: "story",
+    next: "battle1c",
+  },
+  "3a": {
+    text: "Defeating the forest folk, you are able to free them from the influence of Valderak.",
+    type: "story",
+    next: "4a",
+  },
+  "3b": {
+    text: "Defeating the Melwunts, you send them scampering back into the marshes.",
+    type: "story",
+    next: "4b",
+  },
+  "3c": {
+    text: "Defeating the Wretched Dead, they fall to pieces on the floor, no longer animated by Valderak.",
+    type: "story",
+    next: "4c",
+  },
+  "4a": {
+    text: "Your foe leaves behind a powerful artefact: The Wand of Azzerthoth, crackling with violet energies [ATTACK UP]",
+    type: "story",
+    next: "5",
+  },
+  "4b": {
+    text: "Your foe leaves behind a powerful artefact: The Brazen Helm of Caargolos, the demon-mask of a dead god [DEFENSE UP]",
+    type: "story",
+    next: "5",
+  },
+  "4c": {
+    text: "Your foe leaves behind a powerful artefact: The Philtre of Gnomon - a glass vial filled with moonlight [HEALING]",
+    type: "story",
+    next: "5",
+  },
+  "5": {
+    text:
+      "Hidden in the wreckage you find the key to the upper floors. Ascending the winding stairs your ears pick up a harsh, clanking sound. There is smoke upon the air. You are greeted by three doors - to your left a door of ruby, to your right a door of aquamarine, and between, a door of topaz.",
+    type: "story",
+    next: "",
+    choices: [
+      { text: "Proceed Through Ruby Door", action: "6a" },
+      { text: "Proceed Through Topaz Door", action: "6b" },
+      { text: "Proceed Through Aquamarine Door", action: "6c" },
+    ],
+  },
+  "6a": {
+    text:
+      " Opening the ruby door to an upper chamber cloaked in scarlet cloth you encounter Valderak's lieutenant, an Engine of Chaos, powered by the elemental fury of fire.",
+    type: "story",
+    next: "battle2",
+  },
+  "6b": {
+    text:
+      "Opening the topaz door to an upper chamber choked with sand you encounter Valderak's lieutenant, an Engine of Chaos, powered by the elemental fury of lightning",
+    type: "story",
+    next: "battle2",
+  },
+  "6c": {
+    text:
+      "Opening the aquamarine door to an upper chamber riven with streams you encounter Valderak's lieutenant, an Engine of Chaos, powered by the elemental fury of water",
+    type: "story",
+    next: "battle2",
+  },
+  "7a": {
+    text: "The Engine of Chaos is destroyed. From its wreckage you pluck the source of its power, a Gem of Annihilation. It pulses with the power of fire",
+    type: "story",
+    next: "8",
+  },
+  "7b": {
+    text: "The Engine of Chaos is destroyed. From its wreckage you pluck the source of its power, a Gem of Annihilation. It pulses with the power of lightning",
+    type: "story",
+    next: "8",
+  },
+  "7c": {
+    text: "The Engine of Chaos is destroyed. From its wreckage you pluck the source of its power, a Gem of Annihilation. It pulses with the power of water",
+    type: "story",
+    next: "8",
+  },
+  "8": {
+    text:
+      "Using it you dispel the wards and magics protecting the inner sanctum of Valderak. Entering the lair of the Arch-Wizard, you find him, hunched over his scrying table. He turns and addresses you.",
+    type: "story",
+    next: "",
+  },
+  "9a": {
+    text:
+      "All your efforts are in vain, little one. Aquilae will be the first to fall. Your father is doomed - and you shall live just long enough to see him perish!",
+    type: "story",
+    next: "10",
+  },
+  "9b": {
+    text:
+      "Foolish wretch - your tribe is lost in the seas of time! I may not have been able to best their strength, but now they are somewhere their meagre intellect will not save them. Do you truly think one of you can defeat me? After all your people joined as one, failed?",
+    type: "story",
+    next: "10",
+  },
+  "9c": {
+    text:
+      "You dare to disturb the dread Valderak? For what end? To thwart me in my plans? Foolish dolt - you cannot defeat me! All magic is mine! All magic flows through me!",
+    type: "story",
+    next: "10",
+  },
+  "9d": {
+    text:
+      "Wait. I know you. Didn't you break into my tower before? Yes! you're the one who stole my record collection! You took all my precious, priceless Bob Dylan records!",
+    type: "story",
+    next: "10",
+  },
+  "10": {
+    text: "With a screech of disgust he conjures his Poison Blade, and with one final curse on the Four Kingdoms, launches himself at you. Battle is joined.",
+    type: "story",
+    next: "battle3",
+  },
+  "11a": {
+    text:
+      "You return to the Kingdom of Aquilae to find the ravages of the dread Arch-Wizard fading from the land. Your father has been restored to health. With the death of his twin brother Valderak, peace now reigns... for about a fortnight until your evil twin sister escapes...",
+    type: "story",
+    next: "title",
+  },
+  "11b": {
+    text:
+      "In your hand you hold the Dispelling Arch, and returning to the Realm of Harthingfjord, you use it to pluck your tribe from the rivers of time. They are safe, but the Arch demands a sacrifice - you find yourself lost in time and space.",
+    type: "story",
+    next: "title",
+  },
+  "11c": {
+    text:
+      "In your hands the great tome Mallifax belches, swollen with the stolen magics of Valderak. But returning to Fizzlesnip, you find it in disarray. You have done your job too well - all magic has been drained from the Four Kingdoms and beyond. The world is saved but Zazzerpan, you're going to have do what your father's always said. Get a real job.",
+    type: "story",
+    next: "title",
+  },
+  "11d": {
+    text:
+      "You make it through the swamp with your sack of stolen artefacts. Resting for the night you look at your swag. Surely it wouldn't hurt to try on the Ring of Destruction? And maybe the Crown of Pain would compliment it... and the Cloak of Daggers too. Your team-mates raise concerns, but you wave them away, accidentally obliterating them. Your hands course with power. The dark reign of Yolo the Magnificent has begun.",
+    type: "story",
+    next: "title",
+  },
+  "Game Over": {
+    text:
+      "All your valour, all your bravery, was for naught. You have been vanquished. It is a dark day for the Four Kingdoms as its greatest champions lie defeated. There will be many more dark days ahead, for now nothing stands in the way of Valderak's final victory. All is lost.",
+    type: "story",
+  },
+  victory: {
+    text:
+      "With a blood-curdling scream, the magics holding the Arch-Wizard together are sundered for good. In a great rush of cold green fire, the body falls, nothing more than a few rags and bones. The dark tower begins to shudder beneath your feet. Without Valderak to maintain it, it is beginning to sink into the swamp. You fly for the exit, escaping just in time to see the sharp roof sink beneath the waters. The stain on the landscape, the blight on the Four Kingdoms, Valderak the Arch-Wizard is no more.",
+    type: "story",
+    next: "",
+  },
+  battle1a: {
+    type: "battle",
+    enemy: "The Forest People",
+    next: "3a",
+    enemyHealth: 100,
+    enemyStrengh: 10,
+    enemyDefence: 10,
+    enemyMagic: 10,
+    enemyMagicDefence: 10,
+  },
+  battle1b: {
+    type: "battle",
+    enemy: "A Tribe Of Melwunts",
+    next: "3b",
+    enemyHealth: 100,
+    enemyStrengh: 10,
+    enemyDefence: 10,
+    enemyMagic: 10,
+    enemyMagicDefence: 10,
+  },
+  battle1c: {
+    type: "battle",
+    enemy: "The Wretched Dead",
+    next: "3c",
+    enemyHealth: 100,
+    enemyStrengh: 10,
+    enemyDefence: 10,
+    enemyMagic: 10,
+    enemyMagicDefence: 10,
+  },
+  battle2: {
+    type: "battle",
+    enemy: "Engine Of Chaos",
+    next: "",
+    enemyHealth: 100,
+    enemyStrengh: 10,
+    enemyDefence: 10,
+    enemyMagic: 10,
+    enemyMagicDefence: 10,
+  },
+  battle3: {
+    type: "battle",
+    enemy: "Valderak",
+    next: "victory",
+    enemyHealth: 100,
+    enemyStrengh: 10,
+    enemyDefence: 10,
+    enemyMagic: 10,
+    enemyMagicDefence: 10,
+  },
 };
