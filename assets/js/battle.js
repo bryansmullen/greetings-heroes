0bea6f970b2e1f3eb87a64c1b27d5840bbfb9ccd
Bryan Mullen <bryan@bryanmullen.me>
2020-08-02 21:57:50 +0100

    Add Enemy Turn Logic To Battles
    This commit adds the logic for enemys to fight back in battles, as well
    as the logic to trigger the game over screen when the player is defeated

--- assets/js/battle.js
+++ assets/js/battle.js
@@ -1,16 +1,21 @@
 // Battle Logic
+
+import { drawNarrative } from "./application.js";
+import { stage } from "./story.js";
 /**
  * Battle.
  *
 
  */
 export class Battle {
-  constructor(enemy, attackBtn, defendBtn, specialBtn, progressBtn, enemyHealthBarId) {
+  constructor(player, enemy, attackBtn, defendBtn, specialBtn, progressBtn, playerHealthBarId, enemyHealthBarId) {
+    this.player = player;
     this.enemy = enemy;
     this.attackBtn = attackBtn;
     this.defendBtn = defendBtn;
     this.specialBtn = specialBtn;
     this.progressBtn = progressBtn;
+    this.playerHealthBarId = playerHealthBarId;
     this.enemyHealthBarId = enemyHealthBarId;
   }
 
@@ -21,14 +26,17 @@ export class Battle {
   initialiseBattleCommand(attackBtn, defendBtn, specialBtn) {
     attackBtn.addEventListener("click", () => {
       this.playerMove(this.enemy, "enemy-health", "attack");
+      this.computerMove(this.player, "player-health");
       this.checkHealth(this.progressBtn);
     });
     defendBtn.addEventListener("click", () => {
       this.playerMove(enemy, "enemy-health", "attack");
+      this.computerMove(this.player, "player-health");
       this.checkHealth(this.progressBtn);
     });
     specialBtn.addEventListener("click", () => {
       this.playerMove(enemy, "enemy-health", "attack");
+      this.computerMove(this.player, "player-health");
       this.checkHealth(this.progressBtn);
     });
   }
@@ -39,6 +47,7 @@ export class Battle {
         enemy.health -= 10;
         enemyHealthBar = document.getElementById(this.enemyHealthBarId);
         enemyHealthBar.value = (enemy.health / enemy.maxHealth) * 100;
+        console.dir(enemyHealthBar);
         break;
       case "defend":
         console.log("defend");
@@ -48,10 +57,19 @@ export class Battle {
         break;
     }
   }
+  computerMove(player, playerHealthBar) {
+    player.health -= 5;
+    playerHealthBar = document.getElementById(this.playerHealthBarId);
+    console.dir(playerHealthBar);
+    playerHealthBar.value = (player.health / player.maxHealth) * 100;
+  }
+
   checkHealth(progressBtn) {
     console.dir(progressBtn);
     if (this.enemy.health <= 0) {
       document.getElementById(progressBtn).classList.remove("hidden");
+    } else if (this.player.health <= 0) {
+      drawNarrative(stage["Game Over"]);
     }
   }
   run() {
